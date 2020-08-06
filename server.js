50dd3e7ff6fb4ef749116c7059d3bbbeecdee2e1
cbh4270 <62440425+cbh4270@users.noreply.github.com>
2020-08-06 18:22:28 -0400

    startup files/folders

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 2211;
+
+// For serving of static CSS
+// app.use(express.static(__dirname + "/app/css"));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.text());
+// app.use(express.json({ type: "application/vnd.api+json" }));
+
+ app.use(express.static("./app/public"));
+
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(bodyParser.text());
+ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
